be304476b823d34496a6310925aedcb262fe11be
Haricharan <42492595+harichve@users.noreply.github.com>
2020-12-20 16:11:06 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
+
 export class AppComponent {
-  title = 'project';
+  data: any[];
+  valid = false;
+  suggestData: PeriodicElement[];
+  listData: PeriodicElement[];
+  suggestValid = false;
+  sortOrder = 0;
+  errorStatus=false;
+  errorMsg: string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnChange() { }
+
+  toggleSort() {
+    this.sortOrder = 1 - this.sortOrder
+    this.data.sort().reverse();
+  }
+
+  registrationForm = new FormGroup({
+    name: new FormControl('', Validators.required)
+  });
+  onSubmit() {
+    if (this.registrationForm.get('name').value.trim() !== "") {
+      this.http.post("/api/index/addGridData", { "name": this.registrationForm.get('name').value, "sortOrder": this.sortOrder }).subscribe(result => {
+        if (result['status'] === "insertion Sucessful") {
+          this.errorStatus=false;
+          this.data = result['data']
+        } else {
+          this.errorStatus=true;
+          this.errorMsg = result["errorMsg"];
+        }
+      });
+    }else{
+      this.errorStatus=true;
+      this.errorMsg = "Data cannot be empty.";
+    }
+  }
+
+  showSuggesstion() {
+    if (this.registrationForm.value['name'] !== "") {
+      this.http.post<PeriodicElement[]>("/api/index/getGridData", this.registrationForm.value).subscribe(result => {
+        this.listData = []
+        this.suggestValid = true;
+        this.suggestData = result;
+      });
+    } else {
+      this.suggestData = [];
+    }
+  }
+
+  onFocusOut() {
+    this.suggestValid = false;
+  }
+
+  onFocus() {
+    this.suggestValid = true;
+  }
+
+  ngOnInit(): void {
+    this.http.get<PeriodicElement[]>("/api/index/getGridData").subscribe(result => {
+      this.data = result;
+    });
+  }
+}
+export interface PeriodicElement {
+  name: string;
 }
---
+++ src/app/note.ts
@@ -0,0 +1,9 @@
+export interface Note{
+    no:number;
+    label:string;
+    notesMsg:string;
+    importantFlag:boolean;
+    inputDatetime:string;
+    lastUpdateDatetime:string;
+  }
+
